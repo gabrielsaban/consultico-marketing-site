555de8f95dc9eadb3211793b7801f086c4e0233f
gabrielsaban <gabriel.saban@outlook.com>
2026-01-28 16:11:17 +0000

    add thinkfirst section

--- src/sections/HomeSection.tsx
+++ src/sections/HomeSection.tsx
@@ -1,12 +1,14 @@
 import HeroSection from '@/components/HeroSection';
 import MarketingIntro from './MarketingIntro';
+import ThinkFirstSection from './ThinkFirstSection';
 // import KeywordSection from './KeywordSection';
 
 export default function HomeSection() {
   return (
-    <section id="home" className="min-h-screen">
+    <section id="home">
       <HeroSection />
       <MarketingIntro />
+      <ThinkFirstSection />
       {/* <KeywordSection /> */}
     </section>
   );
