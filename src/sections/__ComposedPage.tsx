7c22972544105274a452a9f6ae587712c288631f
gabrielsaban <gabriel.saban@outlook.com>
2026-02-10 17:33:24 +0000

    scroll import

--- src/sections/__ComposedPage.tsx
+++ src/sections/__ComposedPage.tsx
@@ -3,10 +3,12 @@ import ServicesSection from './ServicesSection';
 import ProjectsSocialProof from './ProjectsSocialProof';
 import AboutSection from './AboutSection';
 import ContactSection from './ContactSection';
+import ScrollToSectionOnLoad from '@/components/ScrollToSectionOnLoad';
 
 export default function ComposedPage() {
   return (
     <main className="min-h-screen">
+      <ScrollToSectionOnLoad />
       <HomeSection />
       <ServicesSection />
       <ProjectsSocialProof />
@@ -15,5 +17,4 @@ export default function ComposedPage() {
     </main>
   );
 }
-
 
